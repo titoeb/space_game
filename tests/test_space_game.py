73ae702e4e4c0b2c1ba8a5451ae5643babe0b2d7
titoeb <tim.toebrock@googlemail.com>
2022-04-13 15:23:41 +0000

    refactor tests to avoid unneccessary objects and give better docstrings.

--- tests/test_space_game.py
+++ tests/test_space_game.py
@@ -1,66 +1,40 @@
 import io
 
 import pytest
-
 from space_game import engine
-
-# the actual solution to the game
-SOLUTION = [
-    "2",
-    "2",  # go to sirius and win quiz
-    "1",
-    "42",  # hire copilot on orion
-    "1",
-    "yes",  # go to centauri and buy GPU drive
-    "2",
-    "2",
-    "3",
-    "yes",  # jump into black hole
-]
-
-DEATH_BY_BLACK_HOLE = [
-    "2",
-    "2",  # go to sirius and win quiz
-    "1",
-    "41",  # hire copilot on orion
-    "1",
-    "yes",  # go to centauri and buy GPU drive
-    "1",
-    "2",
-    "3",
-    "yes",  # jump into black hole
-]
-
-# text sniplets that should appear literally in the output
-PHRASES = [
-    "The stars are waiting for you",
-    "Betelgeuse",
-    "credits",
-    "tech-savvy native",
-    "copilot",
-    "buy",
-    "life, the universe and everything",
-    "Black Hole",
-    "stupid idea",
-    "wonders beyond description",
-    "THE END",
-]
 
 
 @pytest.fixture
 def solution_input():
-    """helper function to hijack the keyboard for testing"""
-    return io.StringIO("\n".join(SOLUTION))
+    """Provide the solution of the game into a StringIO object so that it can
+    easily be put into stdin."""
+    return io.StringIO(
+        "\n".join(  # the actual solution to the game
+            [
+                "2",
+                "2",  # go to sirius and win quiz
+                "1",
+                "42",  # hire copilot on orion
+                "1",
+                "yes",  # go to centauri and buy GPU drive
+                "2",
+                "2",
+                "3",
+                "yes",  # jump into black hole
+            ]
+        )
+    )
 
 
 def test_travel(monkeypatch, solution_input):
-    """game finishes"""
+    """Given the solution to the game, test that the game runs through."""
     monkeypatch.setattr("sys.stdin", solution_input)
     engine.run_space_game()
 
 
 def test_output(monkeypatch, capsys, solution_input):
-    """text output is not empty"""
+    """Given the actual solution to the game, test that the
+    games outputs someting on stdout."""
     monkeypatch.setattr("sys.stdin", solution_input)
 
     engine.run_space_game()
@@ -69,9 +43,26 @@ def test_output(monkeypatch, capsys, solution_input):
     assert len(captured.out) > 0
 
 
-def test_die(monkeypatch, capsys):
-    """player dies"""
-    monkeypatch.setattr("sys.stdin", io.StringIO("\n".join(DEATH_BY_BLACK_HOLE)))
+def test_death_by_black_hole(monkeypatch, capsys):
+    """Test that the player dies if he does jump into the blackout without having
+    copilot and hyper-engine."""
+    death_by_black_hole_stdin = io.StringIO(
+        "\n".join(
+            [
+                "2",
+                "2",  # go to sirius and win quiz
+                "1",
+                "41",  # hire copilot on orion
+                "1",
+                "yes",  # go to centauri and buy GPU drive
+                "1",
+                "2",
+                "3",
+                "yes",  # jump into black hole
+            ]
+        )
+    )
+    monkeypatch.setattr("sys.stdin", death_by_black_hole_stdin)
 
     engine.run_space_game()
 
@@ -80,9 +71,25 @@ def test_die(monkeypatch, capsys):
     assert " wonders beyond description" not in captured.out
 
 
-@pytest.mark.parametrize("phrase", PHRASES)
+@pytest.mark.parametrize(
+    "phrase",  # text sniplets that should appear literally in the output
+    [
+        "The stars are waiting for you",
+        "Betelgeuse",
+        "credits",
+        "tech-savvy native",
+        "copilot",
+        "buy",
+        "life, the universe and everything",
+        "Black Hole",
+        "stupid idea",
+        "wonders beyond description",
+        "THE END",
+    ],
+)
 def test_output_phrases(monkeypatch, capsys, solution_input, phrase):
-    """check for some key phrases in the output"""
+    """When running the successful solution, test that the game outputs
+    some expected phrases."""
     monkeypatch.setattr("sys.stdin", solution_input)
 
     engine.run_space_game()
